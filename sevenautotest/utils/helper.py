a7c083459d37ca371795f123b0ca6e57ab40f0e0
seven <45784938+hotswwkyo@users.noreply.github.com>
2021-09-09 15:35:10 +0800

    优化human_readable方法格式化时间

--- sevenautotest/utils/helper.py
+++ sevenautotest/utils/helper.py
@@ -442,11 +442,23 @@ class SevenTimeDelta(datetime.timedelta):
         if lang == "cn":
             mm, ss = divmod(self.seconds, 60)
             hh, mm = divmod(mm, 60)
-            s = "%d:%02d:%02d" % (hh, mm, ss)
+
+            # s = "%d:%02d:%02d" % (hh, mm, ss)
+            if ss > 9:
+                s = "%02d" % ss
+            else:
+                s = "%d" % ss
+            if mm > 0:
+                mf = "%02d分" if mm > 9 else "%d分"
+                s = (mf % mm) + s
+            if hh > 0:
+                s = ("%d小时" % hh) + s
+
             if self.days:
                 s = ("%d 天, " % self.days) + s
             if self.microseconds:
                 s = s + ".%06d" % self.microseconds
+            s = s + "秒"
         else:
             s = str(self)
         return s
